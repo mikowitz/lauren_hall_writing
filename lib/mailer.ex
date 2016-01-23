10275d2af16dc768f70693f0aec59c940c99cb2a
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-01-23 09:44:23 -0500

    I don't even know anymore...

--- lib/mailer.ex
+++ lib/mailer.ex
@@ -1,12 +1,10 @@
 defmodule LaurenHallWriting.Mailer do
-  use Mailgun.Client,
-  domain: Application.get_env(:lauren_hall_writing, :mailgun_domain),
-  key: Application.get_env(:lauren_hall_writing, :mailgun_key)
+  use Mailgun.Client, domain: Application.get_env(:lauren_hall_writing, :mailgun_domain), key: Application.get_env(:lauren_hall_writing, :mailgun_key)
 
   @to "lauren.hall@gmail.com"
   @from "noreply@mg.laurenhallwriting.com"
 
-  def send_contact_email(%{"email" => email, "message" => message, "name" => name}) do
+  def send_contact_email(%{"email" => email, "message" => message, "name" => name}=params) do
     send_email(to: @to,
                from: @from,
                subject: "[laurenhallwriting.com] You have a new message from #{name}",
