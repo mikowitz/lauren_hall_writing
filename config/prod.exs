a00584c29e0bfab3a925772424f6888b126423b9
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-01-23 09:13:40 -0500

    Try non-ssl?

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :lauren_hall_writing, LaurenHallWriting.Endpoint,
   http: [port: {:system, "PORT"}],
-  # url: [host: "example.com", port: 80],
-  url: [scheme: "https", host: "ancient-plateau-14631.herokuapp.com", post: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "ancient-plateau-14631.herokuapp.com", port: 80],
+  # url: [scheme: "https", host: "ancient-plateau-14631.herokuapp.com", post: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
